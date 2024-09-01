9b2dea52758e14fd2ec1b8beae16e6f8591c5949
Pedro Henrique Ferreira <63063916+PedroHenriqueFerreira@users.noreply.github.com>
2024-08-31 23:27:30 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -16,43 +16,29 @@
     2.3 - deadlines[pronto].
 */
 
+// Enumeração de booleanos
 typedef enum { 
     FALSE = 0,
     TRUE = 1,
 } bool;
 
-// Tempo de simulação
-int simulation_time = 0;
-
 // Estrutura de tarefas
 typedef struct {
-    int id;
-    int P;
-    int C;
-    int D;
-    int remaining_c_in_p;
-    int next_p;
+    int id; // Identificador da tarefa
+    int P; // Período de execução
+    int next_P; // Próximo período de execução
+    int C; // Tempo de execução
+    int remaining_C; // Tempo de execução restante
+    int D; // Deadline
+    int remaining_D; // Deadline restante
 } T;
 
-// Vetor de tarefas
-T tasks[100];
-
-// Vetor de tarefas original
-T tasks_original[100];
-
-// Número de tarefas
-int tasks_number = 0;
-
-// Vetor de ID's de tarefas escalonadas pelo RM (Usado para gerar o gráfico)
-int* tasks_scheduled_RM;
-
-// Vetor de ID's de tarefas escalonadas pelo EDF (Usado para gerar o gráfico)
-int* tasks_scheduled_EDF;
-
-// Reseta as tarefas para o próximo algoritmo
-void reset_tasks() {
-    for(int i = 0; i < tasks_number; i++){
-        tasks[i] = tasks_original[i]; 
+// Reseta as tarefas
+void reset_tasks(T* tasks, int tasks_number) {
+    for (int i = 0; i < tasks_number; i++) {
+        tasks[i].remaining_C = tasks[i].C;
+        tasks[i].remaining_D = tasks[i].D;
+        tasks[i].next_P = 0;
     }
 }
 
@@ -67,17 +53,19 @@ int mmc(int a, int b) {
     return a * b / mdc(a, b);
 }
 
-// Calcula o tempo de simulação
-void compute_simulation_time() {
-    simulation_time = tasks[0].P;
+// Calcula o tempo total
+int compute_total_time(T* tasks, int tasks_number) {
+    int total_time = tasks[0].P;
 
-    for(int i = 1; i < tasks_number; i++){
-        simulation_time = mmc(simulation_time, tasks[i].P);
+    for (int i = 1; i < tasks_number; i++) {
+        total_time = mmc(total_time, tasks[i].P);
     }
+
+    return total_time;
 }
 
 // Calcula a soma de Ci/Pi
-float get_sum() {
+float compute_sum(T* tasks, int tasks_number) {
     float sum = 0.0;
 
     for(int i = 0; i < tasks_number; i++){
@@ -88,38 +76,50 @@ float get_sum() {
 }
 
 // Checa a escalonabilidade do RM
-bool check_RM_scalability() {
-    float sum = get_sum();
+bool check_RM_scalability(T* tasks, int tasks_number) {
+    float limit = tasks_number * (pow(2, 1.0 / tasks_number) - 1);
 
-    float test = tasks_number * (pow(2, 1.0 / tasks_number) - 1);
-
-    if(sum <= test) return TRUE;
-    
-    return FALSE;
+    return compute_sum(tasks, tasks_number) <= limit;
 }
 
 // Checa a escalonabilidade do EDF
-bool check_EDF_scalability() {
-    float sum = get_sum();
+bool check_EDF_scalability(T* tasks, int tasks_number) {
+    return compute_sum(tasks, tasks_number) <= 1;
+}
 
-    if (sum <= 1) return TRUE;
-    
-    return FALSE;
+// Atualiza o deadline de cada tarefa
+void update_remaining_D(T* tasks, int tasks_number) {
+    // Reduz o deadline de cada tarefa
+    for (int i = 0; i < tasks_number; i++){
+        if (tasks[i].remaining_D == 0) tasks[i].remaining_D = tasks[i].D;
+        else tasks[i].remaining_D--;
+    }
+}
+
+// Atualiza o tempo de execução da tarefa
+void update_remaining_C(T *task) {
+    // Reduz o tempo de execução da tarefa
+    task->remaining_C--;
+
+    // Se o tempo de execução da tarefa acabou
+    if (task->remaining_C == 0) {
+        // Atualiza o próximo período de execução da tarefa
+        task->next_P += task->P;
+        // Atualiza o tempo de execução da tarefa
+        task->remaining_C = task->C;
+    }
 }
 
 // RM
-void RM() {
-    // Aloca memória para as tarefas escalonadas
-    tasks_scheduled_RM = malloc(simulation_time * sizeof(int));
-
+void RM(T* tasks, int tasks_number, int total_time, int* tasks_schedule_RM) {
     // Para cada unidade de tempo
-    for (int time = 0; time < simulation_time; time++) {
+    for (int time = 0; time < total_time; time++) {
         // Extrai a tarefa com menor período de execução que ainda não foi executada
         T *task_now = NULL;
 
         for (int i = 0; i < tasks_number; i++) {
             // Se a tarefa está no período de execução e ainda tem tempo de execução
-            if (time >= tasks[i].next_p && tasks[i].remaining_c_in_p > 0) {
+            if (time >= tasks[i].next_P && tasks[i].remaining_C > 0) {
                 // Se a tarefa atual é nula ou a tarefa atual tem um período de execução menor
                 if (task_now == NULL || tasks[i].P < task_now->P) {
                     task_now = &tasks[i];
@@ -130,77 +130,55 @@ void RM() {
         // Se a tarefa atual não é nula
         if (task_now != NULL) {
             // Salva na lista de tarefas escalonadas a tarefa atual
-            tasks_scheduled_RM[time] = task_now->id;
-
-            // Reduz o tempo de execução da tarefa
-            task_now->remaining_c_in_p--;
-
-            // Se o tempo de execução da tarefa acabou
-            if (task_now->remaining_c_in_p == 0) {
-                // Atualiza o próximo período de execução da tarefa
-                task_now->next_p += task_now->P;
-                // Atualiza o tempo de execução da tarefa
-                task_now->remaining_c_in_p = task_now->C;
-            }
+            tasks_schedule_RM[time] = task_now->id;
+            // Atualiza o tempo de execução da tarefa
+            update_remaining_C(task_now);
         } else {
             // Salva na lista de tarefas escalonadas -1
-            tasks_scheduled_RM[time] = -1;
+            tasks_schedule_RM[time] = -1;
         }
     }
 }
 
 // EDF
-void EDF() {
-    // Aloca memória para as tarefas escalonadas
-    tasks_scheduled_EDF = malloc(simulation_time * sizeof(int));
-
+void EDF(T* tasks, int tasks_number, int total_time, int* tasks_schedule_EDF) {
     // Para cada unidade de tempo
-    for (int time = 0; time < simulation_time; time++) {
+    for (int time = 0; time < total_time; time++) {
         // Extrai a tarefa com menor deadline que ainda não foi executada
         T *task_now = NULL;
 
         for (int i = 0; i < tasks_number; i++){
             // Se a tarefa está no período de execução e ainda tem tempo de execução
-            if (time >= tasks[i].next_p && tasks[i].remaining_c_in_p > 0) {
+            if (time >= tasks[i].next_P && tasks[i].remaining_C > 0) {
                 // Se a tarefa atual é nula ou a tarefa atual tem um deadline menor
-                if (task_now == NULL || tasks[i].D < task_now->D) {
+                if (task_now == NULL || tasks[i].remaining_D < task_now->remaining_D) {
                     task_now = &tasks[i];
                 }
             }
-
-            // Reduz o deadline de cada tarefa
-            if (tasks[i].D == 0) tasks[i].D = tasks_original[i].D;
-            else tasks[i].D--;
         }
+
+        // Reduz o deadline de cada tarefa
+        update_remaining_D(tasks, tasks_number);
 
         // Se a tarefa atual não é nula
         if(task_now != NULL) {
             // Salva na lista de tarefas escalonadas a tarefa atual
-            tasks_scheduled_EDF[time] = task_now->id;
-
-            // Reduz o tempo de execução da tarefa
-            task_now->remaining_c_in_p--;
-
-            // Se o tempo de execução da tarefa acabou
-            if (task_now->remaining_c_in_p == 0) {
-                // Atualiza o próximo período de execução da tarefa
-                task_now->next_p += task_now->P;
-                // Atualiza o tempo de execução da tarefa
-                task_now->remaining_c_in_p = task_now->C;
-            }
+            tasks_schedule_EDF[time] = task_now->id;
+            // Atualiza o tempo de execução da tarefa
+            update_remaining_C(task_now);
         } else {
             // Salva na lista de tarefas escalonadas -1
-            tasks_scheduled_EDF[time] = -1;
+            tasks_schedule_EDF[time] = -1;
         }
     }
 }
 
 // Mostra o gráfico de tarefas escalonadas
-void graphic(int* tasks_scheduled) {
+void plot(T* tasks, int tasks_number, int total_time, int* tasks_scheduled) {
     for (int task = 0; task < tasks_number; task++) {
         printf("t%d ", tasks[task].id);
 
-        for (int time = 0; time < simulation_time; time++) {
+        for (int time = 0; time < total_time; time++) {
             printf(tasks_scheduled[time] == tasks[task].id ? "#" : "-");
         }
 
@@ -218,66 +196,87 @@ int main(int argc, char *argv[]){
     }
 
     // Abre o arquivo de entrada
-    FILE *input_file;
-    input_file = fopen(argv[1], "r");
+    FILE *file = fopen(argv[1], "r");
 
     // Verifica se o arquivo de entrada é nulo e exibe uma mensagem de erro
-    if (input_file == NULL) {
+    if (file == NULL) {
         perror("diretorio para o arquivo de entrada errado");
         return EXIT_FAILURE;
     }
 
-    // Pula a primeira linha do arquivo de entrada
-    fscanf(input_file, "%*[^\n]\n");
+    // Vetor de tarefas
+    T tasks[100];
 
     // Variáveis para armazenar os valores de P, C e D
     int P, C, D;
 
-    // Variável para armazenar o índice da tarefa
-    int i = 0;
+    // Variável para armazenar a quantidade de tarefas
+    int tasks_number = 0;
+
+    // Pula a primeira linha do arquivo de entrada
+    fscanf(file, "%*[^\n]\n");
 
     // Lê o arquivo de entrada e armazena os valores de P, C e D
-    while (fscanf(input_file, "%d %d %d", &P, &C, &D) != EOF) {
-        tasks[i].id = i + 1;
+    while (fscanf(file, "%d %d %d", &P, &C, &D) != EOF) {
+        tasks[tasks_number].id = tasks_number + 1;
 
-        tasks[i].P = P;
-        tasks[i].C = C;
-        tasks[i].D = D;
-        tasks[i].remaining_c_in_p = C;
-        tasks[i].next_p = 0;
-        tasks_original[i] = tasks[i];
-        i++;
+        tasks[tasks_number].P = P;
+        tasks[tasks_number].C = C;
+        tasks[tasks_number].D = D;
+
+        tasks[tasks_number].remaining_C = C;
+        tasks[tasks_number].remaining_D = D;
+        tasks[tasks_number].next_P = 0;
+
+        tasks_number++;
     }
 
     // Fecha o arquivo de entrada
-    fclose(input_file);
-    
-    // Armazena o número de tarefas
-    tasks_number = i;
+    fclose(file);
 
-    // Calcula o tempo de simulação
-    compute_simulation_time();
+    // Calcula o tempo total de simulação
+    int total_time = compute_total_time(tasks, tasks_number);
 
     // Verifica se o sistema é escalonável pelo RM
-    if (check_RM_scalability() == FALSE) printf("RM falhou no teste de escalonabilidade\n\n");
-    else printf("RM passou no teste de escalonabilidade\n\n");
+    switch (check_RM_scalability(tasks, tasks_number)) {
+        case TRUE:
+            printf("RM passou no teste de escalonabilidade\n\n");
+            break;
+        case FALSE:
+            printf("RM falhou no teste de escalonabilidade\n\n");
+            break;
+    }
+
+    // Vetor com as tarefas escalonadas pelo RM (Usado para gerar o gráfico)
+    int* tasks_schedule_RM = malloc(total_time * sizeof(int));
+
     // Executa o RM
-    RM();
+    RM(tasks, tasks_number, total_time, tasks_schedule_RM);
     // Mostra o gráfico de tarefas escalonadas
     printf("Escalonamento RM: \n");
-    graphic(tasks_scheduled_RM);
+    plot(tasks, tasks_number, total_time, tasks_schedule_RM);
 
     // Reseta as tarefas para o próximo algoritmo
-    reset_tasks();
+    reset_tasks(tasks, tasks_number);
 
     // Verifica se o sistema é escalonável pelo EDF
-    if (check_EDF_scalability() == FALSE) printf("EDF falhou no teste de escalonabilidade\n\n");
-    else printf("EDF passou no teste de escalonabilidade\n\n");
+    switch (check_EDF_scalability(tasks, tasks_number)) {
+        case TRUE:
+            printf("EDF passou no teste de escalonabilidade\n\n");
+            break;
+        case FALSE:
+            printf("EDF falhou no teste de escalonabilidade\n\n");
+            break;
+    }
+
+    // Vetor com as tarefas escalonadas pelo EDF (Usado para gerar o gráfico)
+    int* tasks_schedule_EDF = malloc(total_time * sizeof(int));
+
     // Executa o EDF
-    EDF();
+    EDF(tasks, tasks_number, total_time, tasks_schedule_EDF);
     // Mostra o gráfico de tarefas escalonadas
     printf("Escalonamento EDF: \n");
-    graphic(tasks_scheduled_EDF);
+    plot(tasks, tasks_number, total_time, tasks_schedule_EDF);
 
     return 0;
 }
